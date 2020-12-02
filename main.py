e796654f24efb12921da2162add5f2393236184e
aniket1020 <aniket.1020@gmail.com>
2020-12-02 22:19:17 +0530

    Removed the requirement for temporary files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os,sys,re
 from PIL import Image
+from io import BytesIO
 from pdfrw import PdfReader, PdfWriter
 
 def mergeFiles(inputFilesPaths,outputFilePath):
@@ -7,13 +8,15 @@ def mergeFiles(inputFilesPaths,outputFilePath):
     for path in inputFilesPaths:
         try:
             if re.search('.jpg|.png|.jpeg$',path):
+                file = BytesIO()
                 img=Image.open(path).convert('RGB')
-                img.save('#_tmp_#.pdf',"PDF",resolution=100.0)
-                outputFile.addpages(PdfReader('#_tmp_#.pdf').pages)
+                img.save(file,format="PDF",resolution=100.0)
+                file.seek(0)
+                outputFile.addpages(PdfReader(file).pages)
             else:
                 pdfFile = PdfReader(path)
                 outputFile.addpages(pdfFile.pages)
-        except:
+        except E:
             print(f"Error: {path}")
             return
     with open(outputFilePath,'wb') as out:
@@ -23,7 +26,3 @@ if __name__ ==  '__main__':
     inputFilesPaths = sys.argv[1:-1]
     outputFilePath = sys.argv[-1]
     mergeFiles(inputFilesPaths, outputFilePath)
-    try:
-        os.remove('./#_tmp_#.pdf')
-    except:
-        pass
